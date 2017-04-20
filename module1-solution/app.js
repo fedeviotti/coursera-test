89b5628ec6987260781569d352a7bcf5f8239df3
fedeviotti <fedeviotti@gmail.com>
2017-04-20 22:33:58 +0200

    bonus done

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,13 +9,18 @@ LunchCheckController.$inject = ['$scope', "$filter"];
 function LunchCheckController($scope, $filter) {
   $scope.lunchMenu = "";
   $scope.message = "";
+  $scope.fontColor = "";
 
   $scope.check = function () {
 
     if ($scope.lunchMenu === ""){
       $scope.message = "Please enter data first";
+      $scope.fontColor = "red";
     }else{
       var array = $scope.lunchMenu.split(',');
+      //filter to do NOT consider and empty item towards to the count
+      array = array.filter(function(n){ return n.trim() != "" });
+      $scope.fontColor = "green";
       if(array.length <= 3){
         $scope.message = "Enjoy!";
       }else{
